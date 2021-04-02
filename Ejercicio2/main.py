a55fdbbd39626e644f8bb6a94872f5715f36ae24
Victor Manuel Lara Lopez <vicblack15@hotmail.com>
2021-04-02 15:21:46 -0600

    Create function: numeros_a_romanos, to calculate simbols

--- Ejercicio2/main.py
+++ Ejercicio2/main.py
@@ -1,9 +1,13 @@
-from model import inpt, resta_fechas
+from model import inpt, resta_fechas, numeros_a_romanos
 
 def main():
     lista_fechas = inpt()
     
     numero_convert = resta_fechas(lista_fechas)
+
+    cant_simbolos = numeros_a_romanos(numero_convert)
+
+    print("La cantidad de simbolos que necesitas es: ", cant_simbolos)
 
 if __name__ == "__main__":
     main()
--- Ejercicio2/model.py
+++ Ejercicio2/model.py
@@ -1,3 +1,5 @@
+num = [1,4,5,9,10,40,50,90,100,400,500,900,1000]
+
 def inpt(): # Funcion para introducir datos y regresarlos organizados en una lista
     try:
         fechas = []
@@ -23,12 +25,31 @@ def inpt(): # Funcion para introducir datos y regresarlos organizados en una lis
         print("Error, escribiste mal los datos de entrada")
 
 def resta_fechas(lista_fechas): # Calculamos el tiempo restando las fechas dependiendo 
-
     if lista_fechas[0] == "B" and lista_fechas[2] == "B":
         return lista_fechas[1] - lista_fechas[3]
-
     elif lista_fechas[0] == "B" and lista_fechas[2] == "A":
         return lista_fechas[1] + lista_fechas[3]
-
     else:
         return lista_fechas[3] - lista_fechas[1]
+
+def numeros_a_romanos(numero): # Calculamos la cantidad de simbolos mediante la base 10
+    simbolos = 0
+
+    i = 12 
+    while numero: 
+
+        # Hacemos una division entera tras encontrar su primera base
+        div = numero // num[i]
+
+        # Ahora nuestro numero es lo que queda de sustraer su base
+        numero %= num[i]
+
+        while div:
+
+            # En esta parte vamos adicionando los simbolos que ya encontramos con la division entera
+            simbolos += 1
+            div -= 1
+            
+        i -= 1
+    
+    return simbolos
